d23cebc3a66f223150dfeb0a0b201740805a3e5b
takamitsu-iida <takamitsu.iida@gmail.com>
2025-02-19 09:45:00 +0900

    temp save

--- bin/install_radkit.sh
+++ bin/install_radkit.sh
@@ -3,17 +3,28 @@
 # 実行方法
 # curl -sf https://raw.githubusercontent.com/takamitsu-iida/expt-radkit/refs/heads/main/bin/install_radkit.sh | /bin/bash -s
 
-SCRIPT_FILENAME="cisco_radkit_1.7.6_linux_x86_64.sh"
+RADKIT_VERSION="1.7.6"
+
+SCRIPT_FILENAME="cisco_radkit_${RADKIT_VERSION}_linux_x86_64.sh"
 
 SCRIPT_PATH="http://192.168.0.198/${SCRIPT_FILENAME}"
 
-echo "download RADKit install script to /tmp"
 
-wget -P /tmp ${SCRIPT_PATH}
+if [ ! -e /tmp/${SCRIPT_FILENAME} ]; then
+    # /tmpにまだファイルがなければダウンロードする
+    echo "download RADKit install script to /tmp"
 
-if [ -e /tmp/${SCRIPT_FILENAME} ]; then
-  echo "download failed"
-  exit 1
+    # wgetでファイルを取得して/tmpに保存する
+    wget -P /tmp ${SCRIPT_PATH}
+
+    if [ ! -e /tmp/${SCRIPT_FILENAME} ]; then
+        echo "download failed"
+        exit 1
+    fi
 fi
 
-# sh /tmp/cisco_radkit_1.7.4_linux_x86_64.sh
+# 実行する
+/bin/sh /tmp/${SCRIPT_FILENAME} -- --accept-all --systemd
+
+# ポストインストールを実行する
+/bin/sh /opt/radkit/versions/${RADKIT_VERSION}/post-install.sh
