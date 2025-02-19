e268342f5453dc26f236aa96ac310e781d6d3916
takamitsu-iida <takamitsu.iida@gmail.com>
2025-02-19 09:15:13 +0900

    temp save

--- bin/install_radkit.sh
+++ bin/install_radkit.sh
@@ -3,11 +3,12 @@
 # 実行方法
 # curl -sf https://raw.githubusercontent.com/takamitsu-iida/expt-radkit/refs/heads/main/bin/install_radkit.sh | /bin/bash -s
 
-SCRIPT_FILENAME=cisco_radkit_1.7.6_linux_x86_64.sh
+SCRIPT_FILENAME="cisco_radkit_1.7.6_linux_x86_64.sh"
 
-SCRIPT_PATH=http://192.168.0.198/${SCRIPT_FILENAME}
+SCRIPT_PATH="http://192.168.0.198/${SCRIPT_FILENAME}"
 
 echo "download RADKit install script to /tmp"
+
 wget -P /tmp ${SCRIPT_PATH}
 
 if [ -e /tmp/${SCRIPT_FILENAME} ]; then
